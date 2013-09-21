deb91670b4a98a51a1524b3c92719eef45e085e5
Scott Ballantyne <ussballantyne@gmail.com>
2013-09-21 14:30:55 -0500

    tidy

--- lib/sec_entities/entity.rb
+++ lib/sec_entities/entity.rb
@@ -1,73 +1,63 @@
 module Sec
-    class Entity
+  class Entity
+    attr_accessor :first, :middle, :last, :name, :symbol, :cik, :url, :type, :sic, :location, :state_of_inc, 
+                  :formerly, :mailing_address, :business_address, :relationships, :transactions, :filings
 
-        attr_accessor :first, :middle, :last, :name, :symbol, :cik, :url, :type, :sic, :location, :state_of_inc, :formerly, :mailing_address, :business_address, :relationships, :transactions, :filings
+    def self.find(entity_args, *options)
+      Entity.lookup(Entity.url(entity_args), entity_args)
+    end
 
-        def self.find(entity_args, *options)
-            Entity.cik(Entity.url(entity_args), entity_args)
+
+    def self.query(url)
+      RestClient.get(url){ |response, request, result, &block|
+        case response.code
+          when 200
+          return response
+          else
+          response.return!(request, result, &block)
         end
+      }
+    end
 
 
-        def self.query(url)
-            RestClient.get(url){ |response, request, result, &block|
-                case response.code
-                    when 200
-                    return response
-                    else
-                    response.return!(request, result, &block)
-                end
-            }
+    def self.url(args)
+      if args.is_a?(Hash)
+        if args[:symbol] != nil
+          string = "CIK="+args[:symbol]
+        elsif args[:cik] != nil
+          string = "CIK="+args[:cik]
+        elsif args[:first] != nil and args[:last]
+          string = "company="+args[:last]+" "+args[:first]
+        elsif args[:name] != nil
+          string = "company="+args[:name].gsub(/[(,?!\''"":.)]/, '')
         end
-
-
-        def self.url(args)
-            if args.is_a?(Hash)
-                if args[:symbol] != nil
-                    string = "CIK="+args[:symbol]
-                elsif args[:cik] != nil
-                    string = "CIK="+args[:cik]
-                elsif args[:first] != nil and args[:last]
-                    string = "company="+args[:last]+" "+args[:first]
-                elsif args[:name] != nil
-                    string = "company="+args[:name].gsub(/[(,?!\''"":.)]/, '')
-                end
-            elsif args.is_a?(String)
-                begin Float(args)
-                    string = "CIK="+args
-                rescue
-                    if args.length <= 4
-                        string = "CIK="+args
-                    else
-                        string = "company="+args.gsub(/[(,?!\''"":.)]/, '')
-                    end
-                end
-            end
-            string = string.to_s.gsub(" ", "+")
-            url = "http://www.sec.gov/cgi-bin/browse-edgar?"+string+"&action=getcompany"
-            return url
+      elsif args.is_a?(String)
+        begin Float(args)
+            string = "CIK="+args
+        rescue
+          if args.length <= 4
+            string = "CIK="+args
+          else
+            string = "company="+args.gsub(/[(,?!\''"":.)]/, '')
+          end
         end
+      end
+      string = string.to_s.gsub(" ", "+")
+      url = "http://www.sec.gov/cgi-bin/browse-edgar?"+string+"&action=getcompany"
+      return url
+    end
 
-        def self.cik(url, entity)
-            response = Entity.query(url+"&output=atom")
-            # puts response
-            entries = []
-            Nokogiri::HTML(response).xpath('//feed/entry').each do |e|
-                # e.remove_namespaces!('link')
-                if e.xpath('//content/company-info').to_s.length > 0
-                    doc = Hashie::Mash.new(Crack::XML.parse(e.xpath('//content/company-info').to_s)['company_info'])
-                end
-                if e.xpath('//company-info').to_s.length > 0
-                    doc = Hashie::Mash.new(Crack::XML.parse(e.xpath('//company-info').to_s)['company_info'])
-                end
-                if e.xpath('//content/accession-nunber').to_s.length > 0
-                    doc = Crack::XML.parse(e.xpath('//content').to_s).collect {|k, v| Hashie::Mash.new(v) }
-                end
-                
-                entries << doc
-            end
+    def self.lookup(url, entity)
+      response = Entity.query(url+"&output=atom")
+      entries = []
+      Nokogiri::HTML(response).xpath('//feed/entry').each do |e|
+        doc = Hashie::Mash.new(Crack::XML.parse(e.xpath('//content/company-info').to_s)['company_info']) if e.xpath('//content/company-info').to_s.length > 0
+        doc = Hashie::Mash.new(Crack::XML.parse(e.xpath('//company-info').to_s)['company_info'])         if e.xpath('//company-info').to_s.length > 0
+        doc = Crack::XML.parse(e.xpath('//content').to_s).collect {|k, v| Hashie::Mash.new(v) }          if e.xpath('//content/accession-nunber').to_s.length > 0
+        entries << doc
+      end
 
-            return entries
-        end
-
-   end
+      return entries
+    end
+  end
 end
